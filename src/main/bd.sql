559c593548c3b307b976e88ecdbc509c07af62ab
anna <anna@sayf.su>
2023-06-14 01:49:01 +0300

    выполнено задание spring data

---
+++ src/main/bd.sql
@@ -0,0 +1,14 @@
+CREATE TABLE employee(
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    salary INT NOT NULL
+);
+INSERT INTO employee (name, salary)
+VALUES ('Anna', 150000);
+
+CREATE TABLE position(
+    id_position BIGSERIAL NOT NULL PRIMARY KEY,
+    name_position VARCHAR(50) NOT NULL
+);
+
+ALTER TABLE employee ADD FOREIGN KEY (id_position) REFERENCES  position(id_position);
--- src/main/java/ru/skypro/lessons/springboot/weblibrary/repository/EmployeeRepository.java
+++ src/main/java/ru/skypro/lessons/springboot/weblibrary/repository/EmployeeRepository.java
@@ -1,14 +1,30 @@
 package ru.skypro.lessons.springboot.weblibrary.repository;
 
-import ru.skypro.lessons.springboot.weblibrary.exceptions.EmployeeNotFoundException;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import ru.skypro.lessons.springboot.weblibrary.dto.EmployeeFullInfo;
 import ru.skypro.lessons.springboot.weblibrary.model.Employee;
 
 import java.util.List;
+public interface EmployeeRepository extends CrudRepository<Employee, Integer>, PagingAndSortingRepository<Employee, Integer> {
 
-public interface EmployeeRepository {
-    List<Employee> getAllEmployees();
-    void addEmployee(Employee employee);
-    void editEmployee(String name, Integer salary, int id) throws EmployeeNotFoundException;
-    Employee getEmployeeById(int id) throws EmployeeNotFoundException;
-    void deleteEmployeeById (int id);
+    @Query(value = "SELECT e FROM Employee e")
+    List<Employee> findAllEmployees();
+
+    @Query(value = "FROM Employee e "+
+            "INNER JOIN Position p " +
+            "ON e.position.idPosition = p.idPosition and p.namePosition = :namePosition")
+    List<Employee> getEmployeesByPosition(@Param("namePosition") String position);
+
+    @Query("SELECT new ru.skypro.lessons.springboot.weblibrary.dto." +
+            "EmployeeFullInfo(e.name , e.salary , p.namePosition) " +
+            "FROM Employee e join fetch Position p " +
+            "WHERE e.position = p AND e.id = :id")
+     EmployeeFullInfo getEmployeeFullInfoById(@Param("id") int id);
+
+    @Query(value = "SELECT * FROM employee WHERE salary = (SELECT MAX(salary) from employee)",
+    nativeQuery = true)
+    Employee getEmployeeWithHighestSalary();
 }
